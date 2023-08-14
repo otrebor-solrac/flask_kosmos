f3a9cd096c09820e0a71534b50824dd093ab83ef
rc <solrac_solrac@live.com.mx>
2023-08-14 17:13:43 -0600

    update post_function

--- post_function.py
+++ post_function.py
@@ -6,5 +6,7 @@ with open('text.json', 'r') as archivo:
 
 result = requests.post("http://127.0.0.1:5000", json = data).json()
 
+print("The final solution is save in result.json file: \n",result)
+
 with open('result.json', 'w') as f:
     json.dump(result, f)
